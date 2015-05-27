8acf0a31b6c4cfdb2a86fe0041743836b67ec381
John Root <john.root@digirati.co.uk>
2015-05-27 16:51:29 +0100

    aded setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name=feeds,
+    name=feeds.py,
     version='0.1',
     description='scrape JATS XML to EIF JSON',
     long_description=open('README.md', 'r').read(),
