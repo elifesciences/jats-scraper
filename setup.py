7c43ab0ba01e14ce039945f9ccbd8620e70a264e
John Root <john.root@digirati.co.uk>
2015-05-27 17:11:13 +0100

    aded setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     description='scrape JATS XML to EIF JSON',
     long_description=open('README.md', 'r').read(),
     license=open('gpl.txt', 'r').read(),
-    install_requires=open('requirements.txt', 'r').read().splitlines()
+    install_requires=['scraper', 'elife-tools']
 )
