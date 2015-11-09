5b3b6d7f5fb8ba81be74cb2e36ff12e75a2a1a28
Luke Skibinski <l.skibinski@elifesciences.org>
2015-11-09 10:58:57 +0000

    added install and run-tests scripts

---
+++ run-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source install.sh > /dev/null
+pylint -E elife_ga_metrics/*.py elife_ga_metrics/test/*.py
+python -m unittest discover --verbose --failfast --catch --start-directory tests/ --pattern "*.py"
