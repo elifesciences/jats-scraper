d3115d70f378598ba1140a219503e09b00811e55
Luke Skibinski <l.skibinski@elifesciences.org>
2015-11-09 13:32:29 +0000

    removed the 'failfast' from the test script

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source install.sh > /dev/null
 pylint -E elife_ga_metrics/*.py elife_ga_metrics/test/*.py
-python -m unittest discover --verbose --failfast --catch --start-directory tests/ --pattern "*.py"
+python -m unittest discover --verbose --catch --start-directory tests/ --pattern "*.py"
