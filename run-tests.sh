746870202796ce99fb09c6fb0fce9436b99fe9d2
Luke Skibinski <l.skibinski@elifesciences.org>
2015-11-30 13:56:15 +0000

    disabled pylint - there is some upstream problem that has been released under the stable version causing breakage. for shame pylint.

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source install.sh > /dev/null
-pylint -E tests/*.py
+#pylint -E tests/*.py # disabled 2015-11-30, bug in pylint
 python -m unittest discover --verbose --catch --start-directory tests/ --pattern "*.py"
